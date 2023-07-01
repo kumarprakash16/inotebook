10e6b38ba10749870c044e2dd4813c83a08b1d00
kumarprakash16 <kumarprakash1636@gmail.com>
2023-07-01 11:19:37 +0530

    video 62 completed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Navbar } from "./components/Navbar";
 import { About } from "./components/About";
 import { Home } from "./components/Home";
 import NoteState from "./context/notes/NoteState";
+import { Alert } from "./components/Alert";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <NoteState>
         <Router>
           <Navbar />
+          <Alert message="This is an alert."/>
           <div className="container">
             <Switch>
               <Route exact path="/">
--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from "react";
 
 export const Noteitem = (props) => {
-    const note = props.note;
+  const note = props.note;
   return (
     <>
-    <div className="col-md-3">
-    <div className="card" >
-    <div className="card-body">
-      <h5 className="card-title">{note.title}</h5>
-      <p className="card-text">{note.description}</p><div></div>
-      
-    </div>
-  </div>
-  </div>
-  </>
-  )
-}
+      <div className="col-md-3">
+        <div className="card my-3">
+          <div className="card-body">
+            <div className="d-flex align-items-center">
+              <h5 className="card-title">{note.title}</h5>
+              <i className="fa-solid fa-trash mx-2"></i>
+              <i className="fa-solid fa-file-pen mx-2"></i>
+            </div>
+            <p className="card-text">{note.description}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
